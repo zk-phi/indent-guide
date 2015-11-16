be66dd2e71440f5a5412f8519973298928809c88
p <_>
2015-11-16 18:23:01 +0900

    do not highlight trailing emptylines of blocks

--- indent-guide.el
+++ indent-guide.el
@@ -248,7 +248,8 @@ the point."
               (active-minibuffer-window))
     (let ((win-start (window-start))
           (win-end (window-end nil t))
-          line-col line-start line-end)
+          line-col line-start line-end
+          last-col)
       ;; decide line-col, line-start
       (save-excursion
         (indent-guide--beginning-of-level)
@@ -263,8 +264,11 @@ the point."
                       (forward-line 1)
                       (not (eobp))
                       (<= (point) win-end)))
-          (if (>= line-col (current-column))
-              (forward-line -1))
+          (when (>= line-col (setq last-col (current-column)))
+            (forward-line -1)
+            (while (and (looking-at "[\s\t\n]*$")
+                        (> (point) line-start)
+                        (zerop (forward-line -1)))))
           (setq line-end (line-number-at-pos)))
         ;; draw line
         (dotimes (tmp (- (1+ line-end) line-start))
