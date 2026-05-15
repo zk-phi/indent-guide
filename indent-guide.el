cbe21430a625d292261086610110d731c65b786a
zk-phi <zk-phi@users.noreply.github.com>
2026-05-15 20:50:52 +0900

    Update URL

--- indent-guide.el
+++ indent-guide.el
@@ -17,7 +17,7 @@
 ;; Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 
 ;; Author: zk_phi
-;; URL: http://hins11.yu-yake.com/
+;; URL: http://zk-phi.github.io/
 ;; Version: 2.4
 
 ;;; Commentary:
